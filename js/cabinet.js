d58ee982dbfce9099d0f9e61d878b49e45902538
stikice <stikice@users.noreply.github.com>
2019-11-22 00:20:05 +0600

    cabinet

---
+++ js/cabinet.js
@@ -0,0 +1,190 @@
+var table = $("#train-table");
+
+var row_template = _.template("<tr><td><%=fname%></td>\
+  <td><%=lname%></td>\
+  <td><%=trainid%></td>\
+  <td><%=from%></td>\
+  <td><%=to%></td>\
+  <td><%=wagon%></td>\
+  <td><%=seat%></td>\
+  <td><%=dep%></td>\
+  <td><%=arr%></td>\
+  <td><button class='refund' onclick=makeRefund(<%=ticketid%>)>REFUND</button></td></tr>")
+
+function getName(station_id){
+	const url = globalUrl + 'services/station';
+	$.getJSON(url, function (data) {
+	  $.each(data, function (key, entry) {
+	  	if(entry.id == station_id){
+	  		return entry.name;
+	  	}
+	  });
+	});
+}
+
+function getListItems(){
+  token = $.session.get("auth_token");
+  $.ajax({
+    type: 'get',
+    url : globalUrl + 'services/passenger/tickets',
+    success : function(r) {
+      table.empty();
+      let i = 0;  
+      for(i = 0; i < r.length; i++){
+        let ticket = r[i];
+        console.log(ticket);
+        table.append(row_template({
+          fname: ticket.firstName, 
+          lname: ticket.secondName,
+          trainid: ticket.trainId,
+          from: getName(ticket.stationIdFrom), 
+          to: getName(ticket.stationIdTo),
+          wagon: ticket.wagonNumber,
+          seat: ticket.seatNumber,
+          dep: ticket.departureDatetime, 
+          arr: ticket.arrivalDatetime,
+          ticketid: ticket.id
+        }));
+      }
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#today").offset().top
+      }, 100);
+    },
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    crossDomain : true,
+    dataType : 'json',
+    error: function(r) {
+        alert("Can not get tickets");
+    }
+  });
+}
+
+
+function makeRefund(ticket_id){
+  token = $.session.get("auth_token");
+  $.ajax({
+    type: 'post',
+    url : globalUrl + 'services/passenger/refund-request',
+    data:{
+      "ticket-id" : ticket_id
+    },
+    success : function(r) {
+      alert("You made refund request");
+      $("#submit").prop('disabled','disabled');
+    },
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    crossDomain : true,
+    dataType : 'json',
+    error: function(r) {
+        alert("Refund finished with error");
+    }
+  });
+}
+
+function getRefundRequests(){
+  token = $.session.get("auth_token");
+  $.ajax({
+    type: 'get',
+    url : globalUrl + 'services/agent/refund-requests',
+    success : function(r) {
+      table.empty();
+      let i = 0;  
+      for(i = 0; i < r.length; i++){
+        let ticket = r[i];
+        console.log(ticket);
+        table.append(row_template({
+          fname: ticket.firstName, 
+          lname: ticket.secondName,
+          trainid: ticket.trainId,
+          from: getName(ticket.stationIdFrom), 
+          to: getName(ticket.stationIdTo),
+          wagon: ticket.wagonNumber,
+          seat: ticket.seatNumber,
+          dep: ticket.departureDatetime, 
+          arr: ticket.arrivalDatetime,
+          ticketid: ticket.id
+        }));
+      }
+      $([document.documentElement, document.body]).animate({
+          scrollTop: $("#today").offset().top
+      }, 100);
+    },
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    crossDomain : true,
+    dataType : 'json',
+    error: function(r) {
+        alert("Can not get tickets");
+    }
+  });
+}
+
+function acceptRefund(){
+  token = $.session.get("auth_token");
+  $.ajax({
+    type: 'post',
+    url : globalUrl + 'services/agent/accept-refund',
+    data:{
+      "ticket-id" : ticket_id
+    },
+    success : function(r) {
+      alert("You have accepted refund request");
+    },
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    crossDomain : true,
+    dataType : 'json',
+    error: function(r) {
+        alert("Accept finished with error");
+    }
+  });
+  getRefundRequests();
+}
+
+function rejectRefund(){
+  token = $.session.get("auth_token");
+  $.ajax({
+    type: 'post',
+    url : globalUrl + 'services/agent/reject-refund',
+    data:{
+      "ticket-id" : ticket_id
+    },
+    success : function(r) {
+      alert("You have accepted refund request");
+    },
+    headers: {
+      "Authorization": 'Bearer ' + token
+    },
+    crossDomain : true,
+    dataType : 'json',
+    error: function(r) {
+        alert("Reject finished with error");
+    }
+  });
+  getRefundRequests();
+}
+
+checkType().then( function (){
+  if(userType === "agent"){
+    row_template = _.template("<tr><td><%=fname%></td>\
+  <td><%=lname%></td>\
+  <td><%=trainid%></td>\
+  <td><%=from%></td>\
+  <td><%=to%></td>\
+  <td><%=wagon%></td>\
+  <td><%=seat%></td>\
+  <td><%=dep%></td>\
+  <td><%=arr%></td>\
+  <td><button class='refund' onclick=acceptRefund(<%=ticketid%>)>ACCEPT</button>\
+  <button class='refund' onclick=rejectRefund(<%=ticketid%>)>REJECT</button></td></tr>")
+  getRefundRequests();
+  } else {
+  	getListItems();
+  }
+});
